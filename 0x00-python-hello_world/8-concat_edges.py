24a911b09002384e5c24711d59891e10b7442c69
ChadiElHanchi <133407188+ChadiElHanchi@users.noreply.github.com>
2024-09-25 19:34:39 +0100

    Add files via upload

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[115:120] + str[0:6]
-print(str)
+string = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+string = string[39:67] + string[107:112] + string[:6]
+print(string)
